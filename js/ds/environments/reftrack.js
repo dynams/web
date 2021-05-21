a672f4e57f7abff48a10d85fe68f5da3fd4f4036
Ben Chasnov <bchasnov@uw.edu>
2021-05-20 21:01:37 -0700

    revert

--- js/ds/environments/reftrack.js
+++ js/ds/environments/reftrack.js
@@ -1,83 +1,23 @@
-Array.prototype.add = function(b,c=1) {
-  const a = this, d = [];
-  if( Object.prototype.toString.call( b ) === '[object Array]' ) {
-    if( a.length !== b.length ) {
-      throw "Array lengths do not match.";
-    } else {
-      for(var i = 0; i < a.length; i++) {
-        d[i] = a[i] + c*b[i];
-      }
-    }
-  } else if( typeof b === 'number' ) {
-    for(var i = 0; i < a.length; i++) {
-      d[i] = a[i] + c*b;
-    }
-  }
-  return c;
-}
-
-Array.prototype.mul = function(b) {
-  const a = this, c = [];
-  if( Object.prototype.toString.call( b ) === '[object Array]' ) {
-    if( a.length !== b.length ) {
-      throw "Array lengths do not match.";
-    } else {
-      for(var i = 0; i < a.length; i++) {
-        c[i] = a[i] * b[i];
-      }
-    }
-  } else if(typeof b === 'number') {
-    for(var i = 0; i < a.length; i++) {
-      c[i] = a[i]*b;
-    }
-  }
-  return c;
-}
-
-
-function runge_kutta({ f, t, x, u, dt }) {
-  const dx1 = f(t, x, u ).mul(dt)
-  const dx2 = f(t, x.add(dx1, .5), u).mul(dt)
-  const dx3 = f(t, x.add(dx2, .5), u).mul(dt)
-  const dx4 = f(t, x.add(dx3, .5), u).mul(dt)
-  const dx = dx1.add(dx2, 2).add(dx3, 2).add(dx4, 1).mul(1/6)
-
-  return x.add(dx)
-}
-
-function second_order(x, u, b=1) {
-  const q = x[0];
-  const dq = x[1]
-  const ddq = u;
-  return [ dq, ddq - b*dq ]
-}
-
 function step({ P, S, I }) {
+  let x1 = P.dt*S.x2 + S.x1 // next state
+  const x2 = P.c*I.x - P.d*S.x2 // for some reason when I add this in it starts flashing
+  //x1 = Math.min(Math.max(x1, P.min), P.max)
+  const Sp = { t: S.t+1, x1, x2}
+  const O = { cost: (S.x1 - S.r)**2 }
 
-  const r = 0;
-
-  const f = second_order;
-  const t = S.t
-  const x = [ S.q, S.dq ];
-  const d = 0; // disturbance
-  const u = I.x + d;
-
-
-  const xp = runge_kutta({f, t, x, u, dt:P.dt})
-  const q = xp[0];
-  const dq = xp[1];
-  const Sp = { t: S.t + P.dt, q, dq }
-  const O = { cost: (S.q - S.r)**2 }
-
+  // Ben's code for first order system?
+  // x = S.x + P.dt*I.x
+  // x = Math.min(Math.max(x, P.min), P.max)
+  // Sp = { t: S.t+1, x: x}
   return { Sp , O }
 }
 
 function reset({ P }) {
-  const I = { x:0 }
-  const S = { t:0, d:0, q:0, dq:0 }
+  const I = {x:0}
+  const S = {t:0, x1:0, x2:0}
   const { O } = step({ P, S, I })
 
-  return { P, S, I, O }
+  return { P, S, I, O}
 }
 
 export default { step, reset }
